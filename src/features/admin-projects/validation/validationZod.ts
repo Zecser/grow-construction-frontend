4d64fe6c0e2e94b6efbe90072a738905bd05f13d
aravind <aravind284479@gmail.com>
2025-08-31 22:54:54 +0530

    merged

--- src/features/admin-projects/validation/validationZod.ts
+++ src/features/admin-projects/validation/validationZod.ts
@@ -4,7 +4,7 @@ export const projectSchema = z.object({
     name: z.string().min(1, "Name is required").max(100, "Name cannot exceed 100 characters"),
     description: z.string().min(1, "Detail is required").max(200, "Details cannot exceed 200 characters"),
     date: z.string().min(1, "Date is required"),
-    status: z.string().min(1, "Status is required"),
+    // status: z.string().min(1, "Status is required"),
     statusPercentage: z.string().min(1, "Status Percentage is required"),
     location: z.string().min(1, "Location is required").max(100, "Location cannot exceed 100 characters"),
     addProjectTo: z.string().min(1, "Please select where to add project")
